fa9d20a28aaefbe512dfd7ac8868b4a42ed92151
Christian Hammacher <bmasterc@gmail.com>
2019-09-23 23:04:58 +0200

    Create lite and regular image with GUI

--- stage-dsf/00-system/01-run.sh
+++ stage-dsf/00-system/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+install -m644 files/spidev.conf "${ROOTFS_DIR}/etc/modprobe.d/spidev.conf"
+install -m644 files/minirc.dfl "${ROOTFS_DIR}/etc/minicom/minirc.dfl"
 install files/wpa_supplicant.conf "${ROOTFS_DIR}/boot/wpa_supplicant.conf"
 
 on_chroot << EOF
