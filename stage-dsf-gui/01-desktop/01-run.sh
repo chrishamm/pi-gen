1feae83972564617c2792e4e2be424160714e181
Christian Hammacher <bmasterc@gmail.com>
2019-10-22 01:35:40 +0200

    First official SD image
    VNC server is only installed in the GUI variant (because it requires GUI)
    Use special config.g template for DuetPi
    CUPS service is now disabled by default

--- stage-dsf-gui/01-desktop/01-run.sh
+++ stage-dsf-gui/01-desktop/01-run.sh
@@ -3,6 +3,8 @@
 install -m 644 files/duet3d.png "${ROOTFS_DIR}/usr/share/wallpapers/duet3d.png"
 
 on_chroot << EOF
+systemctl disable cups
+
 update-alternatives --install /usr/share/desktop-base/duet3d.png desktop-background /usr/share/wallpapers/duet3d.png 80
 update-alternatives --set desktop-background /usr/share/wallpapers/duet3d.png
 EOF
--- stage-dsf/01-dsf/02-run.sh
+++ stage-dsf/01-dsf/02-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+install -m 644 files/config.g "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
+
 on_chroot << EOF
 systemctl enable duetcontrolserver
 systemctl enable duetwebserver
