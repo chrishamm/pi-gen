97c2edf739e3067d29b0ead28658222db0c64057
Christian Hammacher <bmasterc@gmail.com>
2020-09-16 16:08:54 +0200

    Do not use prereleases for FAT partition

--- stage-dsf/03-fat/00-run.sh
+++ stage-dsf/03-fat/00-run.sh
@@ -11,12 +11,12 @@ mkdir "${ROOTFS_DIR}/boot/www"
 install files/config.g "${ROOTFS_DIR}/boot/sys/config.g"
 
 # Obtain and install the latest RepRapFirmware files for Duet 3
-RRF_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/RepRapFirmware/releases" | jq '.[0].assets_url' -r`
+RRF_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/RepRapFirmware/releases" | jq 'map(select( .prerelease == false )) | .[0].assets_url' -r`
 RRF_PKG_URL=`curl "$RRF_ASSETS_URL" | jq '.[] | select(.browser_download_url | test("Duet2and3Firmware.*\\\\.zip$")) | .browser_download_url' -r`
 curl -L "$RRF_PKG_URL" | bsdtar -xf - --include "Duet3*" -C "${ROOTFS_DIR}/boot/sys"
 
 # Obtain and install the latest DWC SD package
-DWC_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/DuetWebControl/releases" | jq '.[0].assets_url' -r`
+DWC_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/DuetWebControl/releases" | jq 'map(select( .prerelease == false )) | .[0].assets_url' -r`
 DWC_SD_PKG_URL=`curl "$DWC_ASSETS_URL" | jq '.[] | select(.browser_download_url | test("SD\\\\.zip$")) | .browser_download_url' -r`
 curl -L "$DWC_SD_PKG_URL" | bsdtar -xf - -C "${ROOTFS_DIR}/boot/www"
 
