4995b9493212240574f3c20a9a21db9e5e60bc14
Christian Hammacher <bmasterc@gmail.com>
2020-09-16 17:58:40 +0200

    Fixed permission issue with config.g

--- stage-dsf/01-dsf/02-run.sh
+++ stage-dsf/01-dsf/02-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-install -m 644 files/config.g "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
+install -m 644 files/config.g -o 996 -g 996 "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
 
 on_chroot << EOF
 systemctl enable duetcontrolserver
