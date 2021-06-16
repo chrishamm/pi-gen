4ad3069523f2f14ce02c4aa7fa9ed3b3c76f3dcf
Christian Hammacher <bmasterc@gmail.com>
2021-06-16 18:35:10 +0200

    Version 3.3
    Added new factory image config for in-house testing
    Removed NOOBS build steps again
    Replaced bossac/bossa-gui binaries with v1.9.1 builds
    Bug fix: hostapd service wasn't disabled upon installation
    Bug fix: New DuetPluginService instances were not enabled
    Bug fix: DuetPiMaintenancePlugin wasn't automatically started

--- stage-dsf/00-system/01-run.sh
+++ stage-dsf/00-system/01-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Hopefully this is temporary; Debian still ships outdated bossa versions
+install -m755 files/bossac "${ROOTFS_DIR}/usr/bin/bossac"
+
 install -m644 files/spidev.conf "${ROOTFS_DIR}/etc/modprobe.d/spidev.conf"
 install -m644 files/minirc.dfl "${ROOTFS_DIR}/etc/minicom/minirc.dfl"
 install files/wpa_supplicant.conf "${ROOTFS_DIR}/boot/wpa_supplicant.conf"
@@ -9,6 +12,7 @@ chown pi:root /etc/hostname
 chown pi:root /etc/hosts
 
 systemctl disable dnsmasq
+systemctl disable hostapd
 systemctl disable inetd
 systemctl disable proftpd
 EOF
