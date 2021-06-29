d59f5a5313d15919e7a438ca3a1bfa2689effcd5
Christian Hammacher <bmasterc@gmail.com>
2021-06-29 14:45:34 +0200

    Fixes for v3.3
    Firmware files were unpacked to sys instead of firmware
    FAT partition was missing DuetWiFiServer.bin

--- stage-dsf/03-fat/00-run.sh
+++ stage-dsf/03-fat/00-run.sh
@@ -2,6 +2,7 @@
 
 # Create default RRF directories
 mkdir "${ROOTFS_DIR}/boot/filaments"
+mkdir "${ROOTFS_DIR}/boot/firmware"
 mkdir "${ROOTFS_DIR}/boot/gcodes"
 mkdir "${ROOTFS_DIR}/boot/macros"
 mkdir "${ROOTFS_DIR}/boot/menu"
@@ -14,7 +15,7 @@ install files/config.g "${ROOTFS_DIR}/boot/sys/config.g"
 # Obtain and install the latest RepRapFirmware files for Duet 3
 RRF_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/RepRapFirmware/releases" | jq 'map(select( .prerelease == false )) | .[0].assets_url' -r`
 RRF_PKG_URL=`curl "$RRF_ASSETS_URL" | jq '.[] | select(.browser_download_url | test("Duet2and3Firmware.*\\\\.zip$")) | .browser_download_url' -r`
-curl -L "$RRF_PKG_URL" | bsdtar -xf - --include "Duet3*" -C "${ROOTFS_DIR}/boot/sys"
+curl -L "$RRF_PKG_URL" | bsdtar -xf - --include "Duet3*" --include "DuetWiFiServer.bin" -C "${ROOTFS_DIR}/boot/firmware"
 
 # Obtain and install the latest DWC SD package
 DWC_ASSETS_URL=`curl "https://api.github.com/repos/Duet3D/DuetWebControl/releases" | jq 'map(select( .prerelease == false )) | .[0].assets_url' -r`
