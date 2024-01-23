b81acd38dd350eb778e420f70f970634f9832fc5
Christian Hammacher <bmasterc@gmail.com>
2024-01-23 20:12:57 +0100

    Fixed dsf uid/gid when changing config.g/plugins.txt

--- stage-dsf/01-dsf/02-run.sh
+++ stage-dsf/01-dsf/02-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-install -m 644 files/config.g -o 996 -g 996 "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
-install -m 644 files/plugins.txt -o 996 -g 996 "${ROOTFS_DIR}/opt/dsf/conf/plugins.txt"
+install -m 644 files/config.g -o 992 -g 992 "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
+install -m 644 files/plugins.txt -o 992 -g 992 "${ROOTFS_DIR}/opt/dsf/conf/plugins.txt"
 
 on_chroot << EOF
 systemctl enable duetcontrolserver
@@ -11,4 +11,7 @@ systemctl enable duetpluginservice-root
 
 adduser $FIRST_USER_NAME dsf
 adduser dsf video
+
+chown dsf:dsf /opt/dsf/sd/sys/config.g
+chown dsf:dsf /opt/dsf/conf/plugins.txt
 EOF
