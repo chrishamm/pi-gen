2f447c99f61a59425bc7a4e8defc59c1d8613c5d
Christian Hammacher <bmasterc@gmail.com>
2024-04-19 13:54:56 +0200

    Use stable feed again

--- stage-dsf/07-unstable/01-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-install -m 644 files/plugins.txt -o 992 -g 992 "${ROOTFS_DIR}/opt/dsf/conf/plugins.txt"
-
-on_chroot << EOF
-chown dsf:dsf /opt/dsf/sd/sys/config.g
-chown dsf:dsf /opt/dsf/conf/plugins.txt
-EOF
